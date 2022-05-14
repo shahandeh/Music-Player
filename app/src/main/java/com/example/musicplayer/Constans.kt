aed5c65d199fefbfcea8e6984e7082a617631f0e
majid <majid.shahandeh.ms@gmail.com>
2022-05-14 14:36:20 +0430

    V 0.1 Mini player, Max player, Music list motion layout added WRITE_EXTERNAL_STORAGE permission function added

---
+++ app/src/main/java/com/example/musicplayer/Constans.kt
@@ -0,0 +1,3 @@
+package com.example.musicplayer
+
+const val PERMISSION = 1
---
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.musicplayer
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.musicplayer.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getPermission()
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            Snackbar.make(binding.root, "Permission Granted!", Snackbar.LENGTH_LONG).show()
+        } else {
+            getPermission()
+        }
+    }
+
+    private fun getPermission(){
+        if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), PERMISSION)
+        }
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            val intent = Intent(this, MusicPlayer::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+
+}
